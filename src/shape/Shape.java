b05c118260007949b0bb9603ffb85b38500070e6
wannu <wannu@DESKTOP-KC5PBG6.ht.home>
2022-01-31 01:25:17 -0500

    my first commit

---
+++ src/shape/Shape.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package shape;
+
+/**
+ *
+ * @author Nene Wann
+ */
+public class Shape {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         double radius;
+    }
+
+}
